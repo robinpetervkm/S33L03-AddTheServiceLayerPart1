b76f888f15ad2b1f45adba114b22d39e71f7f53b
Robinclear <robin.peter.vkm@gmail.com>
2020-04-02 18:14:37 -0300

    First Commit

---
+++ src/com/norha/app/DAO/ProductDAOImpl.java
@@ -0,0 +1,36 @@
+package com.norha.app.DAO;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.norha.app.entity.Product;
+@Repository
+public class ProductDAOImpl implements ProductDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<Product> getProducts() {
+		
+		/* Getting session object from the Session Factory */
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		/* Creating Query */
+		Query<Product> theQuery = currentSession.createQuery("from products", Product.class);
+		
+		/* Getting List Of Products */
+		List<Product> products = theQuery.getResultList();
+
+		return products;
+	}
+
+}
